0c58de376e106a07554b3ab1bc72f47d23f63251
darshbaxi <baxidarsh@gmail.com>
2025-02-16 03:34:44 +0530

    minor changes

--- src/server/client.py
+++ src/server/client.py
@@ -2,7 +2,7 @@ import requests
 from typing import Optional, List, Dict, Any
 
 class ZerePyClient:
-    def __init__(self, base_url: str = "http://localhost:8000"):
+    def __init__(self, base_url: str = "https://defai.onrender.com"):
         self.base_url = base_url.rstrip('/')
 
     def _make_request(self, method: str, endpoint: str, **kwargs) -> Dict[str, Any]:
